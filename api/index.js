6cf4152b83494508ef8d0acb522da15e6fbe8c9d
Aleksey Ivanov <lepri4dw@gmail.com>
2024-04-27 01:39:44 +0600

    Added response to frontend

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require("./config");
 const usersRouter = require("./routers/users");
+const OpenAI = require("openai");
+require('dotenv').config();
 
 const app = express();
 const port = 8000;
@@ -11,6 +13,32 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/users', usersRouter);
+
+const openai = new OpenAI({
+  apiKey: 'sk-proj-oFEC89JuRoDI32xF1rVeT3BlbkFJL0DLizwyZaj5xWtNbRDR',
+});
+
+app.post('/ask', async (req, res) => {
+  try {
+    const address = req.body.address;
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: `Какие ближайшие достопримечательности есть рядом с ${address}` }],
+      model: 'ft:gpt-3.5-turbo-0613:personal:guiderv3:9ILEVW8h',
+      max_tokens: 50
+    });
+
+    return res.send({message: 'Success', answer: chatCompletion.choices[0].message.content });
+  } catch (error) {
+    if (error.response) {
+      console.error('Error:', error.response.data);
+    } else if (error.request) {
+      console.error('Error:', error.request);
+    } else {
+      console.error('Error:', error.message);
+    }
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 const run = async () => {
   mongoose.set('strictQuery', false);
--- frontend/src/features/Test.tsx
+++ frontend/src/features/Test.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import axiosApi from "../axiosApi";
 
 const Test: React.FC = () => {
   const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
   const [address, setAddress] = useState('');
+  const [answer, setAnswer] = useState('')
   const [googleLoaded, setGoogleLoaded] = useState(false);
 
   useEffect(() => {
@@ -14,6 +16,20 @@ const Test: React.FC = () => {
     };
     document.body.appendChild(script);
   }, []);
+
+  useEffect(() => {
+    if (address) {
+      axiosApi.post('/ask', { address, answer: String })
+        .then(response => {
+          console.log('Server response:', response.data.answer);
+          setAnswer(response.data.answer);
+        })
+        .catch(error => {
+          console.error('Error sending address:', error);
+        });
+    }
+  }, [address, axiosApi]);
+
 
   const getLocation = () => {
     if (navigator.geolocation) {
@@ -29,7 +45,6 @@ const Test: React.FC = () => {
 
     if (!googleLoaded) return;
 
-    // Use Google Places API to get address
     const geocoder = new window.google.maps.Geocoder();
     const latlng = new window.google.maps.LatLng(latitude, longitude);
     geocoder.geocode({ 'location': latlng }, (results, status) => {
@@ -53,7 +68,8 @@ const Test: React.FC = () => {
       <p>
         Latitude: {location.latitude}<br />
         Longitude: {location.longitude}<br />
-        Address: {address}
+        Address: {address}<br/>
+        Answer: {answer}
       </p>
     </div>
   );
